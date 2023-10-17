acfd9bb25a7a7835c0aade63393270c51a5fd300
P. J. Merlo <52748209+PJMerlo@users.noreply.github.com>
2023-10-17 17:19:56 -0300

    Update CV_javascript.js

--- CV_javascript.js
+++ CV_javascript.js
@@ -7,10 +7,24 @@ document.getElementById("b_mostrar").addEventListener('click', function () {
     document.getElementById("link2").style.display='flex';
     });
     
+document.getElementById("b_mostrar").addEventListener('click', function () { 
+    document.getElementById("link3").style.display='flex';
+    });
+
+document.getElementById("b_mostrar").addEventListener('click', function () { 
+    document.getElementById("link4").style.display='flex';
+    });
 document.getElementById("b_ocultar").addEventListener('click', function () { 
     document.getElementById("link1").style.display='none';
     });
 
 document.getElementById("b_ocultar").addEventListener('click', function () { 
     document.getElementById("link2").style.display='none';
+    });
+document.getElementById("b_ocultar").addEventListener('click', function () { 
+    document.getElementById("link3").style.display='none';
+    });
+    
+document.getElementById("b_ocultar").addEventListener('click', function () { 
+    document.getElementById("link4").style.display='none';
     });
